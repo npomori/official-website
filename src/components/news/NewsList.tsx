96fd4090cdf00bd658a6a409c7b120830eef4ee4
naoki hirata <naoki@magic3.org>
2025-10-02 10:08:13 +0900

    エラーを残したまま一旦コミット。

--- src/components/news/NewsList.tsx
+++ src/components/news/NewsList.tsx
@@ -30,7 +30,8 @@ const NewsList: React.FC = () => {
 
   // 設定を取得
   const config = getConfig()
-  const itemsPerPage = config.pagination?.newsList?.itemsPerPage ?? 10
+  //const itemsPerPage = config.pagination?.newsList?.itemsPerPage ?? 10
+  const itemsPerPage = config.pagination.newsList.itemsPerPage // エラーを発生させる
 
   // 管理権限をチェック
   const hasAdminRole =
