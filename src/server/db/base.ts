e23bb7756d134481603ca52a5de792e0133a1986
naoki hirata <naoki@magic3.org>
2025-11-26 09:24:22 +0900

    Prisma v7対応。

--- src/server/db/base.ts
+++ src/server/db/base.ts
@@ -11,7 +11,8 @@ const prismaInstance =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
+    //log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
+    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error']
   })
 
 if (process.env.NODE_ENV !== 'production') {
